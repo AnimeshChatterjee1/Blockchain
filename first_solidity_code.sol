bd333da80fe707d0b65cf24f03b6e9cb35601969
AnimeshChatterjee1 <91154821+AnimeshChatterjee1@users.noreply.github.com>
2023-05-04 01:56:20 +0530

    Add files via upload

---
+++ first_solidity_code.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FirstContract{
+    //The datatypes in solidity are:
+    //boolean,uint,int,address,bytes
+    bool isfirstcontract=true;
+    int age=19;
+    uint256 lucky_number = 5;
+
+    address my_address = 0x98d4Ff8e9b5E139C58dF60597D10156367Aa57cf;   //can store addresses of account and etc
+    bytes16 mybyte="dog"; 
+} 
